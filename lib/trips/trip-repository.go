01917269e72b89ae50ff7dc0f4d757d7ca7de117
Ethan Young <ethankyoung@gmail.com>
2016-04-30 17:16:27 -0600

    Display the images on the gallery page.

--- lib/trips/trip-repository.go
+++ lib/trips/trip-repository.go
@@ -10,12 +10,20 @@ func NewTripRepository() *TripRepository {
 	t := Trip{
 		Id:   "alaska-2005",
 		Name: "Alaska 2005",
+		Slides: SlidesInfo{
+			IndexFileUrl: "http://images.ethanyoung.us/Alaska2005/Alaska2005Index.xml",
+			RootUrl:      "http://images.ethanyoung.us/Alaska2005",
+		},
 	}
 	allTrips[0] = t
 
 	t = Trip{
 		Id:   "southwest-2007",
 		Name: "Southwest 2007",
+		Slides: SlidesInfo{
+			IndexFileUrl: "http://images.ethanyoung.us/Southwest2007/Southwest2007Index.xml",
+			RootUrl:      "http://images.ethanyoung.us/Southwest2007",
+		},
 	}
 	allTrips[1] = t
 
--- lib/trips/trip.go
+++ lib/trips/trip.go
@@ -1,6 +1,12 @@
 package trips
 
 type Trip struct {
-	Id   string
-	Name string
+	Id     string
+	Name   string
+	Slides SlidesInfo
+}
+
+type SlidesInfo struct {
+	IndexFileUrl string
+	RootUrl      string
 }
