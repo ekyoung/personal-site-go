f8b2b9f2ac15aaa14a8426f6879f2198e82d57e0
Ethan Young <ethankyoung@gmail.com>
2016-05-18 23:18:28 -0600

    Try to write the test reports to the correct folder on linux.

--- server/trips/trips_suite_test.go
+++ server/trips/trips_suite_test.go
@@ -6,10 +6,15 @@ import (
 
 	"github.com/onsi/ginkgo/reporters"
 	"testing"
+
+	"os"
+	"path/filepath"
 )
 
 func TestTrips(t *testing.T) {
 	RegisterFailHandler(Fail)
-	junitReporter := reporters.NewJUnitReporter(`..\..\test-reports\server-trips-junit.xml`)
+	testReportsPath, _ := filepath.Abs("../../test-reports")
+	os.MkdirAll(testReportsPath, 0644)
+	junitReporter := reporters.NewJUnitReporter(filepath.Join(testReportsPath, "server-trips-junit.xml"))
 	RunSpecsWithDefaultAndCustomReporters(t, "Trips Suite", []Reporter{junitReporter})
 }
